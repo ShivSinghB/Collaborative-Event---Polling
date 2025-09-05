3f79749baaa10384c450a7c6808bd21f2f41997e
ShivSinghB <Shivbaghel656@gmail.com>
2025-09-05 12:08:21 +0530

    done

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+// const URI = "mongodb://127.0.0.1:27017/first_mern";
+const URI = process.env.MONGODB_URI;
+
+// mongoose.connect(URI)
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected successfully');
+    console.log("db connected successfully!.........");
   } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
+    console.error("db faild to connect...");
   }
 };
 
 module.exports = connectDB;
 
+// const mongoose = require('mongoose');
 
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB connected successfully');
+//   } catch (error) {
+//     console.error('MongoDB connection error:', error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
